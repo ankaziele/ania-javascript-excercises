4b6b89b43199dcd691a5886c8fa860b13081bb4e
ankaziele <ankaziele@gmail.com>
2018-10-01 12:18:40 +0200

    cars p1

---
+++ cars.js
@@ -0,0 +1,37 @@
+// creating a car
+var road = document.querySelector('.road');
+var car = document.createElement('div')
+car.classList.add('car');
+road.appendChild(car);
+
+//moving right
+var position = 0;
+var velocity = 2;
+
+window.addEventListener('keydown', function (event){
+
+if (event.code === 'ArrowRight' && canMoveRight()) {
+    position += velocity*2;
+    car.style.marginLeft = position + 'px'
+} 
+// moving left
+
+if (event.code === 'ArrowLeft' && canMoveLeft()){
+    position -= velocity*2
+    car.style.marginLeft = position + 'px'
+}
+
+
+})
+
+//don't go outside screen
+
+
+function canMoveRight(){
+   return car.offsetLeft + car.clientWidth <= road.clientWidth
+}
+
+function canMoveLeft(){
+    return car.offsetLeft > road.offsetLeft
+}
+
